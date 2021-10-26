d685d104500220ae55d187f8a390c37e410c80c1
Igor <igormgg@hotmail.com>
2021-10-26 12:02:16 -0300

    feat: reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./reset.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Primeiro contato com TypeScript</h1>
+      </header>
+      <main></main>
     </div>
   );
 }
